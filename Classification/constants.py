8e3c19c1e086902c25de215f50faf451b99bf9ca
Maya Prasad <150702445+prasad1026ma@users.noreply.github.com>
2025-11-27 19:10:56 -0500

    Code refactoring

--- Classification/constants.py
+++ Classification/constants.py
@@ -3,7 +3,7 @@
 CASES_CSV_PATH = 'data/scin_cases.csv'
 LABELS_CSV_PATH = 'data/scin_labels.csv'
 IMAGES_BASE_DIR = 'data/images/'
-OUTPUT_CSV = 'dataset.csv'
+OUTPUT_CSV = 'data/dataset.csv'
 
 CONDITIONS = ['Eczema', 'Allergic Contact Dermatitis', 'Insect Bite',
               'Urticaria','Folliculitis']
